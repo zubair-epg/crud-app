6683aa940b1165aee8ea96cf3128995d142fbbe0
zubair-epg <zubair.epaging@gmail.com>
2022-03-17 05:28:33 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
+import AppBar from './components/read'
+import Create from './components/create'
+import Read from './components/read'
+import Update from './components/update'
 import './App.css';
+import { BrowserRouter as Router,
+   Routes, Route  } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <div>
+       <Routes>
+
+         <Route exact path="/create" element={<Create/>} />
+         <Route exact path="/update" element={<Update/>} />
+         <Route exact path="/" element={<Read/>} />
+
+       </Routes>
+      </div>
+      </Router>
     </div>
   );
 }
---
+++ src/components/create.jsx
@@ -0,0 +1,73 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Container from '@mui/material/Container';
+import { useState } from 'react';
+import axios from 'axios';
+
+export default function Create() {
+
+
+const [firstName , setFirstName] = useState('');
+const [lastName , setLastName] = useState('');
+const [checkBox , setCheckbox] = useState('');
+
+
+ 
+const postData = () =>{
+axios.post(`https://623241cd6f4ffe00fb85929b.mockapi.io/crud`,{
+    firstName,
+    lastName
+})
+
+    console.log(firstName)
+    console.log(lastName)
+    
+}
+
+ var data = axios.get('https://623241cd6f4ffe00fb85929b.mockapi.io/crud')
+console.log(data)
+
+
+  return (
+      <Container max-width="sm">
+
+    
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '35ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+      <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="text" placeholder="Enter firstName" onChange={(e) => setFirstName(e.target.value)} />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="text" placeholder="Enter LastName" onChange={(e) => setLastName(e.target.value)} />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit" onClick={postData} >
+    Submit
+  </Button>
+</Form>
+      </div>
+     
+     
+    </Box>
+    </Container>
+  );
+}
